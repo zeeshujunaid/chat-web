4b78bde1fef9e806c913bb7fe25aefa638f7d50b
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-09-06 01:47:52 +0500

    added loading

--- src/screens/loading.jsx
+++ src/screens/loading.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate, } from "react-router-dom";
 
 function Loading ()  {
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    checkuser()
+  },[])
+
+  const checkuser = async ()=>{
+    const userid = await localStorage.getItem('userid');
+    if (userid !== null) {
+      navigate("/home")
+    }
+    else{
+      navigate("/login")
+    }
+
+    }
+  // checkuser()
+
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="flex items-center space-x-2">
